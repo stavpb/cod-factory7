9b955604940bcf0e27ff1c09cbf41231ecb7afe9
StavPb <stavroula141@gmail.com>
2024-12-10 20:01:43 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * Prints Hello Coding Factory in
+ * standard output (console)
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Coding Factory!");
+    }
+}
